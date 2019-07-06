62fb3cb81aba2fcf130b1d910a66a472293b6619
Patrick Carver <patrickcarver@gmail.com>
2019-07-06 10:02:44 -0500

    rename folders to start with exercise not chp

---
+++ exercise06/retirement_calculator.cr
@@ -0,0 +1,18 @@
+# Retirement Calculator
+
+# Create a program that determines how many years you have left until retirement 
+# and the year you can retire. It should prompt for your current age 
+# and the age you want to retire and display the output as shown 
+# in the example that follows.
+
+# Example Output:
+# ​    What is your current age? 25
+# ​    At what age would you like to retire? 65
+# ​    You have 40 years left until you can retire.
+# ​    It's 2015, so you can retire in 2055. 
+
+# Constraints: 
+# 1.) Again, be sure to convert the input to numerical data before doing any math. 
+# 2.) Don’t hard-code the current year into your program. 
+#     Get it from the system time via your programming language.
+

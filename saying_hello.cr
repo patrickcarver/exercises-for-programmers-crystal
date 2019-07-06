7d29f194a4240c0b44dc548829e7fd39b40fb8d5
Patrick Carver <patrickcarver@gmail.com>
2019-07-05 22:23:47 -0500

    finish chapter 01

---
+++ saying_hello.cr
@@ -0,0 +1,10 @@
+# Saying Hello
+
+# So create a program that prompts for your name and prints a greeting using your name.
+
+# Constraint:
+# Keep the input, string concatenation, and output separate
+
+print "What is your name? "
+name = gets
+puts "Hello, #{name}, nice to meet you!"
